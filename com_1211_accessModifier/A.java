7bba448aa3a7f1ab47dc2b2c47ebbdf919e56b98
Minakshi Deore <minakshideore447@gmail.com>
2026-01-11 19:32:54 +0530

    Add files via upload

---
+++ com_1211_accessModifier/A.java
@@ -0,0 +1,18 @@
+package com_1211_accessModifier;
+
+public class A {
+	public static void main(String[] args) {
+		
+	
+	int i=5432;
+ System.out.println(i);
+System.out.println("5555");
+System.out.println("4444");
+System.out.println("22");
+System.out.println("333");
+	}
+ 
+		
+	}
+
+
